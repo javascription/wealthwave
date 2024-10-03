31857bdd24befd2ddb401e85692c5fbe00fe3004
Gauransh Saini <gauransaini@gmail.com>
2024-10-03 18:02:45 +0530

    final

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -12,10 +12,10 @@ const About1 = () => {
                  About Us
                 </h2>
                 <p className="mb-5 text-base text-text p">
-                At WealthWave, we believe that financial inclusion is the key to unlocking economic potential for individuals and communities alike. Our mission is to provide accessible, innovative, and secure financial solutions to underserved populations. With our user-friendly platform, community-driven microloans, and interactive financial education, we are breaking down the barriers that prevent millions from achieving financial independence.
+                At WealthWave, we believe that financial inclusion is the key to unlocking economic potential for individuals and communities alike. Our mission is to provide accessible, innovative, and secure financial solutions to underserved populations. With our user-friendly platform, interactive financial education, and guided investment portfolio creation driven by our proprietary financial model WeathWave,  we are breaking down the barriers that prevent millions from achieving financial independence.
                 </p>
                 <p className="mb-5 text-base text-text p">
-                By leveraging cutting-edge technology like blockchain and fostering local partnerships, we empower individuals to save, invest, and grow their wealth in a transparent, secure, and sustainable way. Join us as we reshape the future of banking and create opportunities for everyone, everywhere. WealthWave is more than a platform—it’s a movement toward financial empowerment for all.
+                By leveraging cutting-edge technology like blockchain and fostering local partnerships, we empower individuals to save, invest, and grow their wealth in a transparent, secure, and sustainable way. Join us as we reshape the future of investments and create opportunities for everyone, everywhere. WealthWave is more than a platform—it’s a movement toward financial empowerment for all.
                 </p>
               </div>
             </div>
@@ -32,10 +32,10 @@ const About1 = () => {
                  Why Choose Us?
                 </h2>
                 <p className="mb-5 text-base text-text p">
-                WealthWave breaks down the barriers to traditional banking, offering accessible financial services with no hidden fees. Our platform is designed to uplift underserved individuals and small businesses, providing the tools they need to achieve financial independence and sustainable growth.
+                WealthWave breaks down the barriers to financial investments, offering accessible financial services with no hidden fees. Our platform is designed to uplift underserved individuals and small businesses, providing the tools they need to achieve financial independence and sustainable growth by investing even as small as Rs 10 in different financial instruments such as Government Bonds, Sovereign Gold Bonds, Infrastructure Bonds, and Mutual Funds.
                 </p>
                 <p className="mb-8 text-base text-text p">
-                With a focus on community-driven microloans and blockchain-powered security, WealthWave offers cutting-edge financial solutions. We combine transparent lending, interactive education, and local partnerships to empower users, fostering long-term economic stability and prosperity.
+                With a focus on investments in several financial instruments backed by blockchain-powered security, WealthWave offers cutting-edge financial solutions. We combine transparent investments, interactive education, and local partnerships with other financial institutions to empower users, fostering long-term economic stability and prosperity.
                 </p>
               </div>
             </div>
--- src/app/learn/page.js
+++ src/app/learn/page.js
@@ -50,6 +50,50 @@ const BlogSection = () => {
             </div>
           </article>
 
+          {/* Third Article*/}
+          <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <div className="flex justify-between items-center mb-5 text-gray-500">
+              <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
+                    clipRule="evenodd"
+                  ></path>
+                  <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path>
+                </svg>
+                FAQ
+              </span>
+              <span className="text-sm">4 days ago</span>
+            </div>
+            <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              <a href="#">What is Investing? What are financial instruments?</a>
+            </h2>
+            <p className="mb-5 font-light text-gray-500 dark:text-gray-400">
+            Investing is the process of allocating money to assets like stocks, bonds, or real estate with the aim of generating income or growing wealth over time. Financial instruments are assets or contracts, such as stocks, bonds, and derivatives, that can be traded in financial markets. They represent the transfer of capital or the right to future payments.
+            </p>
+            <div className="flex justify-between items-center">
+              <div className="flex items-center space-x-4">
+                <img
+                  className="w-7 h-7 rounded-full"
+                  src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
+                  alt="Bonnie Green avatar"
+                />
+                <span className="font-medium dark:text-white">Bonnie Green</span>
+              </div>
+              <a href="#" className="inline-flex items-center font-medium text-white hover:underline">
+                Read more
+                <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </article>
+
           {/* Second Article */}
           <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
             <div className="flex justify-between items-center mb-5 text-gray-500">
